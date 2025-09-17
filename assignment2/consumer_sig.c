f460322b7ca0c9eac593d584477ef80de822cb81
Logan Nelson <gnelson908@gmail.com>
2025-09-17 17:03:35 +0000

    Separated regular and signal handling versions of consumer and producer.

--- assignment2/consumer_sig.c
+++ assignment2/consumer_sig.c
@@ -4,6 +4,20 @@
 #include <string.h>
 #include <getopt.h>
 #include <time.h>
+#include <signal.h>
+
+volatile sig_atomic_t shutdown_flag = 0; //Triggered by SIGINT
+volatile sig_atomic_t stats_flag = 0; //Triggered by SIGUSR1
+
+void handle_sigint(int sig){
+        shutdown_flag = 1;
+}
+
+void handle_sigusr1(int sig){
+        stats_flag = 1;
+}
+
+
 
 int main (int argc, char *argv[]) {
 	int max_lines = -1; //Unlimited
@@ -11,6 +25,21 @@ int main (int argc, char *argv[]) {
 
 	//Parse arguments (-n max_lines, -v verbose)
 	int opt;
+
+
+	struct sigaction sa_int, sa_usr1;
+
+        //SIGINT Ctrl+C Shutdown
+        sa_int.sa_handler = handle_sigint;
+        sigemptyset(&sa_int.sa_mask);
+        sa_int.sa_flags=0;
+        sigaction(SIGINT, &sa_int, NULL);
+
+        //SIGUSR1 Print stats
+        sa_usr1.sa_handler = handle_sigusr1;
+        sigemptyset(&sa_usr1.sa_mask);
+        sa_usr1.sa_flags = 0;
+        sigaction(SIGUSR1, &sa_usr1, NULL);
 
 	//Loops
 	while ((opt = getopt(argc, argv, "n:v")) != -1) {
@@ -41,12 +70,18 @@ int main (int argc, char *argv[]) {
 	int char_count = 0;
 
 	//Read stdin line by line
-	while((read = getline(&line, &len, stdin)) != -1) {
+	while(!shutdown_flag && (read = getline(&line, &len, stdin)) != -1) {
 		line_count++;
 		char_count += read;
 
 		if(verbose) {
 			printf("%s", line);
+		}
+
+		//Check if SIGUSR 1 was received
+		if(stats_flag){
+			fprintf(stderr, "Lines so far: %d, Characters: %d\n", line_count, char_count);
+			stats_flag = 0; //Reset
 		}
 
 		//Stop after reaching max_lines
