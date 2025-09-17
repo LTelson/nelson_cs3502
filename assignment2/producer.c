7dfa6a928d8d86c4d680612bfb4fd601e0db354a
Logan Nelson <gnelson908@gmail.com>
2025-09-17 01:32:17 +0000

    Updated producer program, added test file and .gitignore

--- assignment2/producer.c
+++ assignment2/producer.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <std.lib.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <getopt.h>
@@ -9,28 +9,30 @@ int main(int argc, char *argv[]) {
 	int buffer_size=4096;
 	char opt;
 
-	while ((opt = getopt(argc,argv,"f:b:")) != 1){
-		switch(opt){
+	while ((opt = getopt(argc,argv,"f:b:")) != -1) {
+		switch(opt) {
 			case 'f':
-			input = fope(optarg,"r");
-			if (!input){
+			input = fopen(optarg,"r");
+			if (!input) {
 				perror("fopen");
 				exit(1);
 			}
 			break;
+
 			case 'b':
 				buffer_size = atoi(optarg);
-				if(buffer_size) <= 0){
+				if(buffer_size <=0) {
 					fprintf(stderr,"Invalid buffer size\n");
 					exit(1);
 				}
 				break;
+
 			default:
 			fprintf(stderr, "Usage: %s [-f filename] [-buffer_size]\n", argv[0]);
 			exit(1);
 	}
-
-
+}
+	
 	char *buffer = malloc(buffer_size);
 	if(!buffer) {
 		perror("malloc");
@@ -46,4 +48,6 @@ int main(int argc, char *argv[]) {
 	fclose(input);
 	}
 	free(buffer);
+
+	return 0;
 }
